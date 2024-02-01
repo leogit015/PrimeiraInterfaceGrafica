67a9c0ecd9ea74e92c13a64f47068f50465afad7
leogit015 <olivercarvalho15@gmail.com>
2024-01-31 22:47:31 -0300

    initial commit

---
+++ InterfaceGrafica/src/PrimeiraInterfaceGrafica/Main.java
@@ -0,0 +1,10 @@
+package PrimeiraInterfaceGrafica;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ InterfaceGrafica/src/PrimeiraInterfaceGrafica/TelaLogin.java
@@ -0,0 +1,52 @@
+package PrimeiraInterfaceGrafica;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+
+public class TelaLogin {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaLogin window = new TelaLogin();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TelaLogin() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		panel.add(lblNewLabel);
+	}
+
+}
